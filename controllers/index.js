7d742fe8c4737cfdfe94743785c93340ba1b1fa3
thecamboprince <dug21002@byui.edu>
2024-01-10 22:46:03 -0700

    first commit (changes to 1st assignment)

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,11 @@
 const awesomeFunction = ((req, res, next) => {
-    res.json('Awesome person');
+    res.json('Viriya Duggan');
 });
 
 const returnAnotherPerson = ((req, res, next) => {
     res.json('Super awesome person');
 });
 
-const week1Assignment = ((req, res, next) => {
-    res.json('Viriya Duggan');
-});
 
 
-module.exports = {awesomeFunction, returnAnotherPerson, week1Assignment};
+module.exports = {awesomeFunction, returnAnotherPerson};
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,5 @@ const myConroller = require('../controllers')
 
 routes.get('/', myConroller.awesomeFunction);
 routes.get('/awesome', myConroller.returnAnotherPerson);
-routes.get('/week1', myConroller.week1Assignment);
 
 module.exports = routes;
